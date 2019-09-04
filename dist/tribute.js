c007ddf25232afa4651e15cf5873c943daf9482a
Step Wright <stephenwright@deltek.com>
2019-09-04 14:12:01 +0100

    undo change
    reverse change to master

--- dist/tribute.js
+++ dist/tribute.js
@@ -1143,8 +1143,8 @@ function () {
           text += _textSuffix;
           this.pasteHtml(text, info.mentionPosition, info.mentionPosition + info.mentionText.length + !this.tribute.autocompleteMode);
         }
-          // this was causing cursor to move to beginning of the 'input' in Chrome, rather than after the insertion
-//         context.element.dispatchEvent(replaceEvent);
+        
+        context.element.dispatchEvent(replaceEvent);
       }
     }
   }, {
